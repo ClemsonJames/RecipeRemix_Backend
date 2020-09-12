e2db5b55233dfe3633ab9c6999856cbf49a2e0ff
ClemsonJames <qingl114@hotmail.com>
2020-09-12 11:50:35 -0400

    api set up

---
+++ controllers/RecipeController.js
@@ -0,0 +1,11 @@
+const spawn = require("child_process").spawn;
+//const pythonProcess = spawn('python',["path/to/script.py", arg1, arg2, ...]);
+
+var RecipeController = {};
+
+RecipeController.createRecipe = function() {
+    
+}
+
+
+module.exports = UserController;
--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const RecipeController = require('../controllers/RecipeController');
+
+
+router.get('/recipe', function(req, res){
+    RecipeController.createRecipe(function(err, user) {
+        if (err){
+            return res.status(400).send(err);
+        }
+        return res.status(200).send(user);
+    });
+    
+})
 
 module.exports = router;
