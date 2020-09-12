59aef0bc5e0888120a02b404bcd7bb343e9db524
ClemsonJames <qingl114@hotmail.com>
2020-09-12 18:00:01 -0400

    meme api

--- controllers/RecipeController.js
+++ controllers/RecipeController.js
@@ -1,13 +1,19 @@
+const path = require('path');
 const spawn = require("child_process").spawn;
 //const pythonProcess = spawn('python',["path/to/script.py", arg1, arg2, ...]);
-
+const fs = require('fs');
 var RecipeController = {};
 
 RecipeController.createRecipe = function(food, callback) {
     return callback(null, {
-        recipe: food + " taste good"
+        recipe: food + ' taste good'
     });
-    
+}
+
+RecipeController.randomMeme = function(callback) {
+    let files = fs.readdirSync(path.join(__dirname, '../asset'));
+    let meme = files[Math.floor(Math.random() * files.length)];
+    return callback(null, path.join(__dirname, '../asset/', meme));
 }
 
 
--- routes/api.js
+++ routes/api.js
@@ -11,11 +11,25 @@ const RecipeController = require('../controllers/RecipeController');
  */
 router.post('/recipe', function(req, res){
     let food = req.body.food;
-    RecipeController.createRecipe(food, function(err, user) {
+    RecipeController.createRecipe(food, function(err, result) {
         if (err){
             return res.status(400).send(err);
         }
-        return res.status(200).send(user);
+        return res.status(200).send(result);
+    });
+    
+})
+
+/**
+ * GET REQUEST
+ * get a random meme
+ */
+router.post('/meme', function(req, res){
+    RecipeController.randomMeme(function(err, fileName) {
+        if (err){
+            return res.status(400).send(err);
+        }
+        return res.status(200).sendFile(fileName);
     });
     
 })
