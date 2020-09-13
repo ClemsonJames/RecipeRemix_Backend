6184f68ca229fc6c81ce7daba23b7904b49eef5c
ClemsonJames <qingl114@hotmail.com>
2020-09-13 10:00:59 -0400

    added https support

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
+// start http server
+const port = process.env.PORT || 3000;
+// http
+//let server = http.createServer(app).listen(port);
+
+// start https server
+let sslOptions = {
+   key: fs.readFileSync('./server.key'),
+   cert: fs.readFileSync('./server.crt')
+};
 
 //route
 const api = require('./routes/api');
 app.use('/api', api)
 
-app.listen(port, () => {
+// https 
+https.createServer(sslOptions, app).listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});
+})
